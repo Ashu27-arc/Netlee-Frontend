3332bb307129742ac548f9381f0c8e3c12a48d8a
Ashutosh Rathor <111623370+Ashu27-arc@users.noreply.github.com>
2025-11-24 13:43:37 +0530

    ok-update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://192.168.1.12:5000/api",
+    baseURL: "https://netlee-backend.onrender.com/api",
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
